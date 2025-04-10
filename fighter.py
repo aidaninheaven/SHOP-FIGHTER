77df57e347f3bdfa03e5a527c2b0fdd903ce05c9
shahooda1 <158490210+shahooda1@users.noreply.github.com>
2025-04-10 09:42:33 -0400

    Update fighter.py

--- fighter.py
+++ fighter.py
@@ -6,6 +6,25 @@ class Fighter():
 
         self.rect = pygame.Rect((x, y, 80, 180))
 
+    def move(self):
+
+        speed = 10
+        dx = 0
+        dy = 0
+
+        #get keypresses
+        key = pygame.key.get_pressed()
+
+        #movement
+        if key[pygame.K_a]:
+            dx = -speed
+        if key[pygame.K_d]:
+            dx = speed
+
+        #update player position
+        self.rect.x += dx
+        self.rect.y += dy
+
     def draw(self, surface):
 
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
