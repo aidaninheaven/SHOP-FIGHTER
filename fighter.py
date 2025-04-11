f9bdad8a6b0e52276411bd4df754fa34aa0deb56
shahooda1 <158490210+shahooda1@users.noreply.github.com>
2025-04-11 09:45:59 -0400

    Update fighter.py

--- fighter.py
+++ fighter.py
@@ -1,5 +1,6 @@
 import pygame
-from projectile import Projectile
+
+from projectiles import Projectile
 
 class Fighter():
     
@@ -10,7 +11,15 @@ class Fighter():
         #Y velocity
         self.velY = 0
 
-    def move(self, screenWidth, screenHeight):
+
+        self.jump = False
+        self.attacking = False
+        self.facingLeft = False
+
+        #which attack is being used
+        self.attackType = 0
+
+    def move(self, screenWidth, screenHeight, surface, target):
 
         speed = 10
         gravity = 2
@@ -20,14 +29,34 @@ class Fighter():
         #get keypresses
         key = pygame.key.get_pressed()
 
-        #movement
-        if key[pygame.K_a]:
-            dx = -speed 
-        if key[pygame.K_d]:
-            dx = speed
-        #jump
-        if key[pygame.K_w]:
-            self.velY = -30
+        #can only perform other actions if not currently attacking
+        if self.attacking == False:
+
+            #movement
+            if key[pygame.K_a]:
+                dx = -speed
+                self.facingLeft = True
+
+            if key[pygame.K_d]:
+                dx = speed
+                self.facingLeft = False
+            #jump
+            if key[pygame.K_w] and self.jump == False:
+                self.velY = -30
+                self.jump = True
+
+            #attack
+            if key[pygame.K_r] or key[pygame.K_t] or key[pygame.K_f]:
+
+                self.attack(surface, target)
+                
+                #determine which attack was used
+                if key[pygame.K_r]:
+                    self.attackType = 1
+                if key[pygame.K_t]:
+                    self.attackType = 2
+                if key[pygame.K_f]:
+                    self.attackType = 3
 
         #apply gravity
 
@@ -46,17 +75,36 @@ class Fighter():
         if self.rect.bottom + dy > screenHeight - 110:
 
             self.velY = 0
+            self.jump = False
             dy = screenHeight - 110 - self.rect.bottom
 
         #update player position
         self.rect.x += dx
         self.rect.y += dy
 
-    #firing funct
+    def attack(self, surface, target):
+
+        self.attacking = True
+
+        attackingRect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
+
+        if attackingRect.colliderect(target.rect):
+            print("Hit")
+
+
+
+        pygame.draw.rect(surface, (0, 255, 0), attackingRect)
+
     def fire(self):
-        direction = -1 if self.facing_left else 1
-        return Projectile(self.rect.centerx, self.rect.centery, direction)
-    
+
+        if self.facingLeft == True:
+
+            direction = -1
+
+            electroBall = Projectile(self.rect.centerx, self.rect.y, direction)
+
+        
+
     def draw(self, surface):
 
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
