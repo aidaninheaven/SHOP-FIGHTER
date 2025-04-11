a71276783539fa707510edafa4a1bda1305ab0a8
viejop <158598912+viejop@users.noreply.github.com>
2025-04-11 09:11:54 -0400

    Update projectiles.py

--- projectiles.py
+++ projectiles.py
@@ -1,1 +1,10 @@
+class Projectile:
+      def __init__(self, x, y, direction):
+        self.rect = pygame.Rect(x, y, 20, 10)
+        self.speed = 12 * direction
 
+    def update(self):
+        self.rect.x += self.speed
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, (0, 0, 255), self.rect)
