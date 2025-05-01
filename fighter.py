821ba867072b1d36babca12fd19ddb0b8a3b952f
shahooda1 <158490210+shahooda1@users.noreply.github.com>
2025-05-01 16:49:26 -0400

    Update fighter.py

--- fighter.py
+++ fighter.py
@@ -4,18 +4,25 @@ from projectiles import Projectile
 
 class Fighter():
     
-    def __init__(self, x, y, data, spriteSheet, animationSteps):
+    def __init__(self, x, y, flip, data, spriteSheet, animationSteps):
 
         self.size = data[0]
+        self.imageScale = data[1]
+        self.offset = data[2]
 
-        self.flip = False
+        self.flip = flip
 
         self.animationList = self.loadImages(spriteSheet, animationSteps)
+        self.action = 0 #0: idle   #1: run     #2: jump    #3: attack1    #4: attack2    #5: hit    #6: death
+        self.frameIndex = 0
+        self.image = self.animationList[self.action][self.frameIndex]
+        self.updateTime = pygame.time.get_ticks()
 
         self.rect = pygame.Rect((x, y, 80, 180))
 
         #Y velocity
         self.velY = 0
+        self.running = False
         self.jump = False
         self.attacking = False
        
@@ -36,8 +43,9 @@ class Fighter():
             tempImgList = []
 
             for x in range(animation):
-                tempImg = spriteSheet.subsurface(x * self.size, 0, self.size, self.size)
-                tempImgList.append(tempImg)
+                tempImg = spriteSheet.subsurface(x * self.size, y * self.size, self.size, self.size)
+                
+                tempImgList.append(pygame.transform.scale(tempImg, (self.size * self.imageScale, self.size * self.imageScale)))
 
             animationList.append(tempImgList)
 
@@ -52,6 +60,8 @@ class Fighter():
         gravity = 2
         dx = 0
         dy = 0
+        self.running = False
+        self.attackType = 0
 
         #get keypresses
         key = pygame.key.get_pressed()
@@ -63,10 +73,12 @@ class Fighter():
             if key[pygame.K_a]:
                 dx = -speed
                 self.facingLeft = True
+                self.running = True
 
             if key[pygame.K_d]:
                 dx = speed
                 self.facingLeft = False
+                self.running = True
             #jump
             if key[pygame.K_w] and self.jump == False:
                 self.velY = -30
@@ -84,6 +96,8 @@ class Fighter():
                     self.attackType = 2
                 if key[pygame.K_f]:
                     self.attackType = 3
+
+        print(self.attackType)
 
         #apply gravity
 
@@ -115,6 +129,41 @@ class Fighter():
         self.rect.x += dx
         self.rect.y += dy
 
+    #handle animation updates
+    def update(self):
+        
+        #check what action the player is performing
+
+        if self.attacking == True:
+            if self.attackType == 1:
+                self.updateAction(3) #3: attack1
+            elif self.attackType == 2:
+                self.updateAction(4) #4: attack2
+        
+        if self.jump == True:
+            self.updateAction(2) #2: jump
+
+        elif self.running == True:
+            self.updateAction(1) #1: run
+
+        else:
+            self.updateAction(0) #0: idle
+
+
+        animationCooldown = 50
+
+        #update image
+        self.image = self.animationList[self.action][self.frameIndex]
+
+        #check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.updateTime > animationCooldown:
+            self.frameIndex += 1
+            self.updateTime = pygame.time.get_ticks()
+
+        #check if the animation has finished
+        if self.frameIndex >= len(self.animationList[self.action]):
+            self.frameIndex = 0
+
     def attack(self, surface, target):
 
         self.attacking = True
@@ -131,8 +180,18 @@ class Fighter():
         pygame.draw.rect(surface, (0, 255, 0), attackingRect)
 
 
+    def updateAction(self, newAction):
+        #check if the new action is different to the previous one
+        if newAction != self.action:
+            self.action = newAction
+
+            #update the animation settings
+            self.frameIndex = 0
+            self.updateTime = pygame.time.get_ticks()
 
 
     def draw(self, surface):
 
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.imageScale), self.rect.y - (self.offset[1] * self.imageScale)))
