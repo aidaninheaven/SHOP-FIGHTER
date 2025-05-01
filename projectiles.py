7fb2261de2fa7eb8d2c2e730602a31b801a24e28
viejop <158598912+viejop@users.noreply.github.com>
2025-05-01 09:01:36 -0400

    Update projectiles.py

--- projectiles.py
+++ projectiles.py
@@ -1,12 +1,31 @@
 import pygame
 
 class Projectile:
-    def __init__(self, x, y, direction):
-        self.rect = pygame.Rect(x, y, 20, 10)
-        self.speed = 12 * direction
+    def __init__(self, x, y, radius, color, facing):
+        #initialize the projectile w position size color direction and speed
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing  #adjust velocity based on direction projectile is facing
+        self.is_active = True  #to determine if projectile's still active
+        self.max_distance = 600  #maximum distance projectile can travel
+        self.travelled = 0  #keeps track of how far the projectile has traveled
 
-    def update(self):
-        self.rect.x += self.speed
+    def move(self):
+        #moves the projectile left or right based on direction
+        if self.is_active:
+            self.x += self.vel  #move based on velocity
+            self.travelled += abs(self.vel)  #increase the distance tracker
 
-    def draw(self, surface):
-        pygame.draw.rect(surface, (0, 0, 255), self.rect)
+            #deactivate the projectile if it travels farther than maximum distance
+            if self.travelled > self.max_distance:
+                self.is_active = False
+
+    def draw(self, win):
+        #draws projectile as circle instead of rect
+        if self.is_active:
+            pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+
